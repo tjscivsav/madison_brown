95e5aec5d2fb8dad94e47ac8de538aaf7bc33f5c
karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-02-01 19:27:58 +0530

    auto hide added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,21 +9,40 @@ import "../../styles/bootstrap.css"
 export default function Home() {
   return (
     <>
-      <section className="bg_gif d-flex flex-column">
-        <img src={logo} width={243} height={162} alt="" />
-        <img src={iceCream} width={330} height={326} alt="" />
-        <form className="form" action="">
-          <div class="input-group mb-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="example@xyz.com"
-            />
-            <button class="btn btn-outline-secondary" type="button">
-              Sign Up
-            </button>
-          </div>
-        </form>
+      <section className=" d-flex flex-column">
+        <div id="hide" className="bg_gif"></div>
+        <div className="content d-flex flex-column">
+          <img src={logo} width={243} height={162} alt="" />
+          <img src={iceCream} width={330} height={326} alt="" />
+          <form
+            className="form"
+            action="https://madisonbrownicecream.us14.list-manage.com/subscribe/post?u=cc8239a65f07d2b1871ac509f&amp;id=a2d5a9b530"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            target="_blank"
+            novalidate
+          >
+            <div class="input-group mb-3">
+              <input
+                type="email"
+                name="EMAIL"
+                id="mce-EMAIL"
+                placeholder="Email Address"
+                name="b_cc8239a65f07d2b1871ac509f_a2d5a9b530"
+                className="input text-center"
+                required
+              />
+              <input
+                type="submit"
+                value="Sign Up"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+                class="button"
+              />
+            </div>
+          </form>
+        </div>
       </section>
     </>
   )
