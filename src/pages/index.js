0db5c756bb46876b6c734634c4a5e78e4ec6c8d0
karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-02-01 22:07:20 +0530

    toastify and mail fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import useWindowSize from "../../hooks/useWindowSize"
 import iceCream from "../../static/ICE_cream_Product.gif"
 import logo from "../../static/logo.png"
 import "../../styles/global.css"
 import "../../styles/resposive.css"
 import "../../styles/bootstrap.css"
+import addToMailchimp from "gatsby-plugin-mailchimp"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function Home() {
+  const [email, setEmail] = useState("")
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const result = await addToMailchimp(email)
+    if (result.result === "success") {
+      toast(result.msg, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    } else {
+      toast(`${result.msg}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setEmail("")
+  }
+
+  const handleChange = e => {
+    console.log({
+      [`${e.target.name}`]: e.target.value,
+    })
+    setEmail(e.target.value)
+  }
+
   return (
     <>
       <section className=" d-flex flex-column">
@@ -14,33 +52,29 @@ export default function Home() {
         <div className="content d-flex flex-column">
           <img src={logo} width={243} height={162} alt="" />
           <img src={iceCream} width={330} height={326} alt="" />
-          <form
-            className="form"
-            action="https://madisonbrownicecream.us14.list-manage.com/subscribe/post?u=cc8239a65f07d2b1871ac509f&amp;id=a2d5a9b530"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            target="_blank"
-            novalidate
-          >
-            <div class="input-group mb-3">
-              <input
-                type="email"
-                name="EMAIL"
-                id="mce-EMAIL"
-                placeholder="Email Address"
-                name="b_cc8239a65f07d2b1871ac509f_a2d5a9b530"
-                className="input text-center"
-                required
-              />
-              <input
-                type="submit"
-                value="Sign Up"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                class="button"
-              />
-            </div>
+          <form className="form" onSubmit={e => handleSubmit(e)}>
+            <input
+              type="email"
+              defaultValue={email}
+              onChange={e => handleChange(e)}
+              placeholder="Email Address"
+              name="email"
+              className="input text-center"
+            />
+            <input type="submit" className="button" value="Sign Up" />
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+            {/* Same as */}
+            <ToastContainer />
           </form>
         </div>
       </section>
