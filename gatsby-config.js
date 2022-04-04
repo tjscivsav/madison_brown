844673021eb20ff9f4b13e4d0420c1bf27086271
Karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-04-04 20:38:22 +0530

    config changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
- const path = require(`path`)
+const path = require(`path`)
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -17,13 +17,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
