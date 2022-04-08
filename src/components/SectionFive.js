bac9f587391ada1115d936cd8314f4c3e064dc7c
Karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-04-08 15:57:38 +0530

    made changes in red frame height for varies screen

--- src/components/SectionFive.js
+++ src/components/SectionFive.js
@@ -1,11 +1,56 @@
 import React from "react"
 import * as five from "../../styles/SectionFive.module.css"
 import productData from "../../site/data/products.json"
+import useWindowDimensions from "../../hooks/useDimensions"
+import useWindowSize from "../../hooks/useWindowSize"
 
 function SectionFive() {
+  const { height } = useWindowDimensions()
+  const { width } = useWindowSize()
+  let x = width / height
+  //  .5622, .462, .462, .4618, .486, .450, .695, 0.75, 0.667, 0.72, .451, 1.707, 1.6
+  let border_height
+  switch (true) {
+    case x < 0.405:
+      border_height = height * 0.13
+      break
+    case x < 0.465:
+      border_height = height * 0.109
+      break
+    case x < 0.49:
+      border_height = height * 0.142
+      break
+    case x < 0.6667:
+      border_height = height * 0.06
+      break
+    case x < 0.7:
+      border_height = height * 0.07
+      break
+    case x === 0.75 && height < 730:
+      border_height = height * 0.16
+      break
+    case x < 0.76:
+      border_height = height * 0.09
+      break
+    case x < 1.61:
+      border_height = height * 0.15
+      break
+    case x < 1.76:
+      border_height = height * 0.25
+      break
+    case x < 1.82:
+      border_height = height * 0.15
+      break
+    case x < 1.85:
+      border_height = height * 0.155
+      break
+    default:
+      border_height = 160
+  }
+  console.log(x, width, height, border_height)
   return (
     <>
-      <div className={five.border_img}>
+      <div style={{ height: `${border_height}vh` }} className={five.border_img}>
         <div className="container-fluid">
           <div className="row">
             <div className={`col-lg-6 col-md-12 col-12 ${five.img_section}`}>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -94,7 +94,7 @@ function Contact() {
                 <div className={contactStyle.content_section}>
                   <div className={contactStyle.detail_section}>
                     <h3>Contact Us</h3>
-                    <div className="d-flex align-items-center ">
+                    {/* <div className="d-flex align-items-center ">
                       <img
                         width={100}
                         height={100}
@@ -102,6 +102,17 @@ function Contact() {
                         alt=""
                       />
                       <a href="tel:707-541-5674">707-541-5674</a>
+                    </div> */}
+                    <div className="d-flex align-items-center ">
+                      <img
+                        width={55}
+                        height={45}
+                        src="https://res.cloudinary.com/dq4fvmcte/image/upload/v1648564411/Madison%20Brown/mail_icon_cfqkkv.png"
+                        alt=""
+                      />
+                      <a href="mailto:Sales@madisonbrown.com">
+                        Sales@madisonbrown.com
+                      </a>
                     </div>
                     <div className="d-flex align-items-center ">
                       <img
@@ -110,9 +121,11 @@ function Contact() {
                         src="https://res.cloudinary.com/dq4fvmcte/image/upload/v1648564411/Madison%20Brown/mail_icon_cfqkkv.png"
                         alt=""
                       />
-                      <a href="mailto:707-541-5674">Madisonbrown@gmail.com</a>
+                      <a href="mailto:Marketing@madisonbrown.com">
+                        Marketing@madisonbrown.com
+                      </a>
                     </div>
-                    <div className="d-flex align-items-center ">
+                    {/* <div className="d-flex align-items-center ">
                       <img
                         width={55}
                         height={45}
@@ -122,7 +135,7 @@ function Contact() {
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                       </p>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
