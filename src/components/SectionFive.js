9d48673b7e7875ac54d4cf417c00aef3fc3e45b6
Karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-04-13 20:47:02 +0530

    footer links removed

--- src/components/SectionFive.js
+++ src/components/SectionFive.js
@@ -1,53 +1,8 @@
 import React from "react"
-import * as five from "../../styles/sectionFive2.module.css"
+import * as five from "../../styles/sectionFive.module.css"
 import productData from "../../site/data/products.json"
-import useWindowSize from "../../hooks/useWindowSize"
 
 function SectionFive() {
-  const { width, height } = useWindowSize()
-  console.log(useWindowSize())
-  let x = width / height
-  let border_height
-  switch (true) {
-    case x < 0.405:
-      border_height = height * 0.13
-      break
-    case x < 0.465:
-      border_height = height * 0.109
-      break
-    case x < 0.49:
-      border_height = height * 0.142
-      break
-    case x < 0.563:
-      border_height = height * 0.15
-      break
-    case x < 0.6667:
-      border_height = height * 0.06
-      break
-    case x < 0.7:
-      border_height = height * 0.07
-      break
-    case x === 0.75 && height < 730:
-      border_height = height * 0.16
-      break
-    case x < 0.76:
-      border_height = height * 0.09
-      break
-    case x < 1.61:
-      border_height = height * 0.15
-      break
-    case x < 1.76:
-      border_height = height * 0.25
-      break
-    case x < 1.82:
-      border_height = height * 0.15
-      break
-    case x < 1.85:
-      border_height = height * 0.155
-      break
-    default:
-      border_height = 170
-  }
   return (
     <>
       <div className={five.border_img}>
