15396a56f31399d53a04dac16a533f45f1fcddd6
Karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-04-29 23:15:14 +0530

    button disabled

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -8,18 +8,20 @@ function Poster({ data }) {
     <>
       <div className={poster.bg_img}>
         <h2>{data?.title ? data?.title : `FIND OUR ICE CREAM`}</h2>
-        <Link
-          target="_blank"
-          to={
-            data.poster_btn_link
-              ? data.poster_btn_name
-              : "https://www.walmart.com/"
-          }
-        >
-          {data?.poster_btn_name
-            ? data?.poster_btn_name
-            : `Available now at Walmart.com`}
-        </Link>
+        {data?.poster_btn_name ? (
+          <Link
+            target="_blank"
+            to={
+              data.poster_btn_link
+                ? data.poster_btn_name
+                : "https://www.walmart.com/"
+            }
+          >
+            {data?.poster_btn_name
+              ? data?.poster_btn_name
+              : `Available now at Walmart.com`}
+          </Link>
+        ) : null}
       </div>
     </>
   )
