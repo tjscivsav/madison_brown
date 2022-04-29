08c268e2e0c2078b8e2cd87388c794c0d69e41d5
Karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-04-29 21:02:33 +0530

    link and responsive added

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -8,7 +8,14 @@ function Poster({ data }) {
     <>
       <div className={poster.bg_img}>
         <h2>{data?.title ? data?.title : `FIND OUR ICE CREAM`}</h2>
-        <Link to={data.poster_btn_link ? data.poster_btn_name : ""}>
+        <Link
+          target="_blank"
+          to={
+            data.poster_btn_link
+              ? data.poster_btn_name
+              : "https://www.walmart.com/"
+          }
+        >
           {data?.poster_btn_name
             ? data?.poster_btn_name
             : `Available now at Walmart.com`}
